7af574d18bf31a625d6796c9a53d8653a5c7adfb
yaredghebre <yared2391@gmail.com>
2023-04-26 14:44:01 -0300

    Add: AppHeader (Day 1)

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,15 +1,79 @@
 <script>
-    export default {
-        name: "AppHeader",
+import Jumbotron from './Jumbotron.vue';
+
+export default {
+    name: "AppHeader",
+    components: {
+        Jumbotron
+    },
+    data() {
+        return {
+            links: [
+                {
+                    text: "HOME",
+                    url: "#",
+                    current: true,
+                },
+                {
+                    text: "ABOUT US",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "FACILITIES",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "MEMBERSHIP",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "TESTIMONIALS",
+                    url: "#",
+                    current: false,
+                },
+                {
+                    text: "BLOG",
+                    url: "#",
+                    current: false,
+                },
+            ]
+        }
     }
+}
+
 </script>
 
 <template>
-    
+    <header class="d-flex justify-content-between align-items-center p-5">
+        <div class="logo">
+            <img src="../assets/images/gym_logo_1x.png" alt="Header Logo">
+        </div>
+        <nav>
+            <ul class="nav-links text-white d-flex gap-5">
+                <li v-for="link in links" :class="link.current ? 'active' : ''">
+                    <a :href="link.url">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <Jumbotron />
 </template>
 
 <style scoped lang="scss">
 @use "../style/general.scss";
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixins" as *;
+
+header {
+    width: 100%;
+    z-index: 100;
+    position: fixed;
+    .active {
+        color: $secondary_color;
+    }
+}
+
 </style>
---
+++ src/components/Jumbotron.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+    name: "Jumbotron"
+}
+</script>
+
+<template>
+    <div class="jumbo-container text-white d-flex flex-column align-items-center justify-content-center">
+        <h1 class="">NO PAIN NO GAIN</h1>
+        <p>Motivation is what gets you started. Habit is what keeps you going.</p>
+        <div class="link-box">
+            <button type="button" class="btn btn-outline-light px-4 py-2">JOIN US TODAY</button>
+        </div>
+    </div>
+</template>
+
+<style scoped lang="scss">
+@use "../style/general.scss";
+@use "../style/partials/variables" as *;
+
+.jumbo-container {
+    background-image: url(../assets/images/home-banner.jpg);
+    height: 1000px;
+    position: relative;
+    top: -50px;
+    
+        h1 {
+            font-weight: 700;
+            font-size: 6.5rem;
+            letter-spacing: 5px;
+        }
+
+        button {
+            font-size: 1.3rem;
+        }
+}
+</style>
